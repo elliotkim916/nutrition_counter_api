da4885397727861e952685f7ef8e799ef613da95
Elliot Kim <elliotkim916@gmail.com>
2018-11-10 17:24:51 -0800

    Added server

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/api/*', (req, res) => {
+  res.json({ok: true});
+});
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+
+module.exports = {app};
