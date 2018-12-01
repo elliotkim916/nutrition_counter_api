b299bb7ff57555f2cfec861d574171e1f30f793b
Elliot Kim <elliotkim916@gmail.com>
2018-11-30 16:39:51 -0800

    Added runServer, closeServer, and mounted exerciseRouter & nutritionRouter

--- server.js
+++ server.js
@@ -2,13 +2,59 @@
 
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
-const PORT = process.env.PORT || 3000;
+const exerciseRouter = require('./routes/exercise.js');
+const nutritionRouter = require('./routes/nutrition.js');
 
-app.get('/api/*', (req, res) => {
-  res.json({ok: true});
-});
+const {PORT, DATABASE_URL} = require('./config');
+//const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+// app.get('/api/*', (req, res) => {
+//   res.json({ok: true});
+// });
 
-module.exports = {app};
+//app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+
+app.use('/api/exercise', exerciseRouter);
+app.use('/api/nutrition', nutritionRouter);
+
+let server;
+
+function runServer (databaseUrl, port=PORT) {
+  return new Promise((resolve, reject) => {
+    return mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(PORT, () => {
+        console.log(`Listening on ${PORT}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+function closeServer () {
+  return mongoose.disconnect.then(() => {
+    return new Promise((resolve, reject) => {
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => err);
+}
+
+module.exports = {app, runServer, closeServer};
