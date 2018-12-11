27a09e28bae76473a6c1c1564d7182c315a499a9
Elliot Kim <elliotkim916@gmail.com>
2018-12-10 19:16:06 -0800

    Added   - UserSchema   - users router and when making POST request, checks all fields are provided, fields are strings, fields are correct length, fields dont start or end with whitespace   - users router can hash password and create new user

---
+++ users/router.js
@@ -0,0 +1,115 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {User} = require('./models');
+
+// Post to register a new user
+router.post('/', jsonParser, (req, res) => {
+  const requiredFields = ['username', 'password'];
+  const missingField = requiredFields.find(field => !(field in req.body));
+  if (missingField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidiationError',
+      message: 'Missing Field',
+      location: missingField
+    });
+  }
+
+  // check that all of the fields are strings
+  const stringFields = ['username', 'password', 'firstName', 'lastName'];
+  const nonStringField = stringFields.find(field => field in req.body && typeof req.body[field] !== 'string');
+  if (nonStringField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Incorrect field type: expected string',
+      location: nonStringField
+    });
+  }
+
+  // check that the username & password neither start or end with whitespace
+  const explicitlyTrimmedFields = ['username', 'password'];
+  const nonTrimmedField = explicitlyTrimmedFields.find(field => req.body[field].trim() !== req.body[field]);
+  if (nonTrimmedField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Cannot start or end with whitespace'
+    });
+  }
+
+  // check that the username & password are the correct length
+  const sizedFields = {
+    username: {
+      min: 1
+    },
+    password: {
+      min: 8,
+      max: 72
+    }
+  };
+  const tooSmallField = Object.keys(sizedFields).find(
+    field => 
+      'min' in sizedFields[field] && 
+      req.body[field].trim().length < sizedFields[field].min
+  );
+  const tooLargeField = Object.keys(sizedFields).find(
+    field =>
+      'max' in sizedFields[field] &&
+      req.body[field].trim().length > sizedFields[field].max
+  );
+  if (tooSmallField || tooLargeField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: tooSmallField 
+        ? `Must be at least ${sizedFields[tooSmallField].min} characters long`
+        : `Must be at most ${sizedFields[tooLargeField].max} characters long`,
+      location: tooSmallField || tooLargeField
+    });
+  }
+
+  let {username, password, firstName = '', lastName = ''} = req.body;
+  firstName = firstName.trim();
+  lastName = lastName.trim();
+
+  return User.find({username})
+    .count()
+    .then(count => {
+      if (count > 0) {
+        // When there is an existing user with the same username
+        return Promise.reject({
+          code: 422,
+          reason: 'ValidationError',
+          message: 'Username already taken',
+          location: 'username'
+        });
+      }
+      // If there is no existing user, hash the password
+      // Hashing is a process that converts a raw, plain text password to a string of unguessable characters
+      return User.hashPassword(password);
+    })
+    .then(hash => {
+      User.create({
+        username,
+        password: hash,
+        firstName,
+        lastName
+      });
+    })
+    .then(user => {
+      return res.status(201).json(user.serialize());
+    })
+    .catch(err => {
+      if (err.reason === 'ValidationError') {
+        return res.status(err.code).json(err);
+      }
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
