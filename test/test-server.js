0092e487024d8946014add3ead86a6c77829248b
Elliot Kim <elliotkim916@gmail.com>
2018-12-12 19:05:25 -0800

    Added test for GET requests

--- test/test-server.js
+++ test/test-server.js
@@ -1,20 +1,96 @@
 'use strict';
 
+const mongoose = require('mongoose');
 const chai = require('chai');
 const chaihttp = require('chai-http');
-
-const {app} = require('../server');
-
-const should = chai.should();
 chai.use(chaihttp);
+// const should = chai.should();
+const expect = chai.expect;
 
-describe('API', function() {
-  it('should 200 on GET requests', () => {
-    return chai.request(app)
-      .get('/api/fooooo')
-      .then(res => {
-        res.should.have.status(200);
-        res.should.be.json;
-      });
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
+
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+const {ExerciseList} = require('../models/exercise');
+const testData = require('../test_db/exercise');
+
+function teardownDb() {
+  return mongoose.connection.dropDatabase();
+}
+
+const createAuthToken = user => {
+  return jwt.sign({user}, config.JWT_SECRET, {
+    subject: user.username,
+    expiresIn: config.JWT_EXPIRY,
+    algorithm: 'HS256'
   });
+};
+
+const TEST_USER = {
+  'username': 'exercise-test-user',
+  'password': 'exercise-test-password' 
+};
+
+const TEST_TOKEN = createAuthToken(TEST_USER);
+
+describe('Nutrition Counter Server Side API', function() {
+  before(function() {
+    return runServer(TEST_DATABASE_URL);
+  });
+
+  beforeEach(function() {
+    return ExerciseList.insertMany(testData);
+  });
+
+  afterEach(function() {
+    return teardownDb();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  describe('GET endpoint', function() {
+    it('should return all of users exercise information', function() {
+      return chai.request(app)
+        .get(`/api/exercise/${TEST_USER.username}`)
+        .set('Authorization', `Bearer ${TEST_TOKEN}`)
+        .then(res => {
+          expect(res).to.be.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('array');
+          expect(res.body).to.have.lengthOf.at.least(1);
+        });
+    });
+
+    let resExercisePost;
+    it('should return users exercise post with all of the right fields', function() {
+      return chai.request(app)
+        .get(`/api/exercise/${TEST_USER.username}`)
+        .set('Authorization', `Bearer ${TEST_TOKEN}`)
+        .then(res => {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('array');
+          expect(res.body).to.have.lengthOf.at.least(1);
+
+          res.body.forEach(post => {
+            expect(post).to.be.a('object');
+            expect(post).to.include.keys('CaloriesBurned', 'MET', 'Duration', 'username');
+          });
+
+          resExercisePost = res.body[0];
+          return ExerciseList.findById(resExercisePost._id);
+        })
+        .then(post => {
+          expect(resExercisePost.CaloriesBurned).to.equal(post.CaloriesBurned);
+          expect(resExercisePost.MET).to.equal(post.MET);
+          expect(resExercisePost.Duration).to.equal(post.Duration);
+          expect(resExercisePost.username).to.equal(post.username);
+        });
+    });
+  });
+
 });
