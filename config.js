ee279b8efab2bcba9cc72058583a21f55fdb5f36
Elliot Kim <elliotkim916@gmail.com>
2019-02-06 00:12:48 -0800

    Updated client origin

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN;
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/nutrition_counter';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-nutrition_counter';
