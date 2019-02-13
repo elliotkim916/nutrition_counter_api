ca3db218140a5f229ab392f21e5616a07304d850
Elliot Kim <elliotkim916@gmail.com>
2019-02-12 17:18:27 -0800

    Changed schema, made more simple

--- models/nutrition.js
+++ models/nutrition.js
@@ -6,32 +6,24 @@ const mongoose = require('mongoose');
 
 // For each Mongoose model we create, we define a schema. A schema specifies how all documents in a particular collection should look. 
 const nutritionSchema = mongoose.Schema({
-  Calories : {type: Number},
-  Total_Fat : {type: Number},
-  Saturated_Fat : {type: Number},
-  Cholesterol : {type: Number},
-  Sodium : {type: Number},
-  Potassium : {type: Number},
-  Carbohydrates : {type: Number},
-  Dietary_Fiber : {type: Number},
-  Sugars : {type: Number},
-  Protein : {type: Number},
+  calories : {type: Number},
+  fat : {type: Number},
+  carbs : {type: Number},
+  protein : {type: Number},
+  sugar : {type: Number},
+  sodium : {type: Number},
   username : {type: String}
 });
 
 // serialize method, which lets us specify how nutrition is represented outside of our application via our API. 
 nutritionSchema.methods.serialize = () => {
   return {
-    Calories : this.Calories,
-    Total_Fat : this.Total_Fat,
-    Saturated_Fat : this.Saturated_Fat,
-    Cholesterol : this.Cholesterol,
-    Sodium : this.Sodium,
-    Potassium : this.Potassium,
-    Carbohydrates : this.Carbohydrates,
-    Dietary_Fiber : this.Dietary_Fiber,
-    Sugars : this.Sugars,
-    Protein : this.Protein,
+    calories : this.calories,
+    fat : this.fat,
+    carbs : this.carbs,
+    protein : this.protein,
+    sugar : this.sugar,
+    sodium : this.sodium,
     username : this.username,
     id : this._id
   };
