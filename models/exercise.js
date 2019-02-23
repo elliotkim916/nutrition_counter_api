4c75db62d3e716c40e58067ed7414a48a4438900
Elliot Kim <elliotkim916@gmail.com>
2019-02-22 17:43:26 -0800

    Updated ExerciseList model

--- models/exercise.js
+++ models/exercise.js
@@ -3,19 +3,19 @@
 const mongoose = require('mongoose');
 
 const exerciseSchema = mongoose.Schema({
-  CaloriesBurned: {type: Number},
-  MET: {type: Number},
-  Duration: {type: String},
-  username: {type: String}
+  caloriesBurned : {type: Number},
+  duration : {type: Number},
+  created : {type: Date, default: Date.now},
+  username : {type: String}
 });
 
 exerciseSchema.methods.serialize = () => {
   return {
-    CaloriesBurned: this.CaloriesBurned,
-    MET: this.MET,
-    Duration: this.Duration,
-    username: this.username,
-    id: this._id
+    caloriesBurned : this.caloriesBurned,
+    duration : this.duration,
+    created : this.created,
+    username : this.username,
+    id : this._id
   };
 };
 
